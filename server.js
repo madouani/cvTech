dd91d26db85982d965bf3eafb96c6d858a3156c1
adouani <mahmoud.adouani@hotmail.com>
2022-01-01 23:49:13 +0000

    config

--- server.js
+++ server.js
@@ -5,11 +5,10 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static('./dist/cvTech'));
+app.use(express.static('./dist/cv-tech/index.html'));
 
 app.get('/*', (req, res) =>
-    res.sendFile('index.html', {root: 'dist/angular-app-heroku/'}),
+    res.sendFile('index.html', {root: 'dist/cv-tech/cvtech-angular/'}),
 );
-
 // Start the app by listening on the default Heroku port
 app.listen(process.env.PORT || 8080);
