2416f5f3123f781542ea426ee5352ff856ee4c4f
adouani <mahmoud.adouani@hotmail.com>
2022-01-05 13:02:51 +0000

    v3

--- src/app/observable/observable.component.ts
+++ src/app/observable/observable.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { min, Observable } from 'rxjs';
+
 
 @Component({
   selector: 'app-observable',
@@ -7,9 +9,43 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ObservableComponent implements OnInit {
 
+  monObservable: Observable<any>;
+  mesImages = [
+    'lateral1.jpg','lateral2.jpg'
+  ];
+  currentImage: any;
   constructor() { }
 
   ngOnInit(): void {
-  }
+    console.log(this.mesImages[0]);
+    console.log(this.mesImages[1]);
+   this.monObservable= new Observable(observer => {
+      let i=this.mesImages.length-1;
+      setInterval(() => {
+        observer.next(this.mesImages[i]);
+        if(i>0){
+          i--;
+       }else {
+         i=this.mesImages.length-1;
+     }
+      }, 2000);
 
+
+
+    });
+
+    this.monObservable.subscribe( val => {
+      console.log(val);
+      this.currentImage= val; // first subscription
+    });
+
+    this.monObservable.subscribe( val2 => {
+      console.log('Second subscription'); // second subscription
+      this.currentImage= val2;
+    });
+
+  }
 }
+
+
+
